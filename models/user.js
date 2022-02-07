dd5692f6d276913f6b50c6f2f1484c2bdec0fde6
Molly Wobbles <molly.rendon@outlook.com>
2022-02-07 15:37:40 -0500

    Set up file structure and updated models files

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init();
+
+module.exports = User;
