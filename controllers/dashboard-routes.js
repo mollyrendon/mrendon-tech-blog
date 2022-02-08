3e06e2e7fe32cb715b9daad2215750d10d87e235
Molly Wobbles <molly.rendon@outlook.com>
2022-02-08 15:49:32 -0500

    Updated routes connections, updated home-routes.js fie, finished dashboard-routes.js file, and added comments to all

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,85 @@
+/*Required Connections*/
+
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("models");
+const withAuth = require("../utils/auth");
+
+/*Find All Function:
+This code is used to find all posts that have been created by the user.  It will check if the loggedIn property is ture, if it is then it will render the dashboard page with all of the posts on it.
+*/
+router.get("/", withAuth, (req, res) => {
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("dashboard", { posts, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+/*Edit Function:
+This code is used to find a post with a spefic ID and edit said post.  If the post with the specific ID is not found a 404 error message is returned.  If the requested post/comment is found then the user will be redirected to the edit page.  Like the other functions the
+user must be logged in (loggedIn: true) to be able to access the data.     
+*/
+
+router.get("/edit/:id", withAuth, (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "There is not a post with this id" });
+        return;
+      }
+      const post = dbPostData.get({ plain: ture });
+      res.render("edit-post", { post, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+router.get("/new", (req, res) => {
+  res.render("new-post");
+});
+
+module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,8 +4,8 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
-/*Post findAll:
-This code is used to render the homepage with posts and loggedIn as a parameter.  
+/*Find All Function:
+This code is used to find all posts that have been created by the user.  The code will check to see if loggedIn is true before rendering anything on the homepage.  This is an e
 */
 router.get("/", (req, res) => {
   Post.findAll({
@@ -34,3 +34,102 @@ router.get("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+/*Login and Signup:
+This Login code is used to show the user a login page when they are not logged in.  It checks to see if the user is logged in, if they are it redirects them to the homepage.  The Signup code is used to show
+the user a signpage page when they are not signed up.  
+*/
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+/*Find ID Function:
+This code looks for a post with a specified ID, if it is unable to find the post with the specific ID it returns a 404 error.  Otherwise it gets the data from that post and renders it to the user if they are logged in.  
+*/
+
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "content", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "There is not a post with this id" });
+        return;
+      }
+      const post = dbPostData.get({ plain: true });
+      res.render("single-post", { post, loggedIn: req.session, loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+/*Posts Comments Function:
+This code is used to find a post with a specified ID.  If there is not a post with that specific ID then a 404 error message is returned.  Otherwise it gets the data from the database and renders the 
+data to the user if they are logged in.
+*/
+
+router.get("/posts-comments", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "content", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "There is not a post with this id" });
+        return;
+      }
+      const post = dbPostData.get({ plain: true });
+
+      res.render("posts-comments", { post, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
