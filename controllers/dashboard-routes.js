08fc74e89302aed904f0b9a34ab0c37483f877f0
Molly Wobbles <molly.rendon@outlook.com>
2022-02-09 18:11:25 -0500

    Updated gitignore file and fixed typo

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,7 +2,7 @@
 
 const router = require("express").Router();
 const sequelize = require("../config/connection");
-const { Post, User, Comment } = require("models");
+const { Post, User, Comment } = require("../models");
 const withAuth = require("../utils/auth");
 
 /*Find All Function:
