9e09682116c90c46f06b5327bbd88f9734e2f3cf
Molly Wobbles <molly.rendon@outlook.com>
2022-02-11 13:22:46 -0500

    Updated comments

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -1,5 +1,5 @@
 /*Required Connections*/
-const { Post } = require("../models");
+const { Post } = require('../models');
 
 /*Post Data:
 This code creates the posts in the database.
@@ -9,13 +9,13 @@ const postData = [
     title: "Fusce non erat dapibus",
     content:
       "Praesent vel quam id sem varius iaculis sit amet sodales nulla. Phasellus vel diam non massa pretium viverra eget tempor tortor. Vivamus sagittis ante lorem, at egestas felis commodo at. ",
-    user_id: 1,
+    user_id: 1
   },
   {
     title: "Etiam cursus cursus nisi",
     content:
       "Vivamus non erat lobortis, lobortis tortor non, euismod ante. Ut sit amet pharetra sapien. Sed finibus aliquet efficitur. Nullam ut rutrum mauris. Pellentesque at augue consectetur, tempus nulla at, consectetur sapien. ",
-    user_id: 2,
+    user_id: 2
   },
   {
     title: "Curabitur non finibus tortor",
